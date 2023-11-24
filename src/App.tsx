fcd6acad088d8bce115bb5fcce33b57f75ce503d
Lorenzo Ferranti <lorenzoferranti04@outlook.it>
2023-11-24 11:25:50 +0100

    prototipo GestioneLista

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GestioneLista from './components/gestione_lista/GestioneLista'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <GestioneLista titolo="Titolo Lista"/>
+    </div>
+  )
+}
+
+export default App
