30d80028dfc1bdb7250c55508466e1a7415fb553
Andrea Denina <andredeninaf@gmail.com>
2023-11-26 18:15:29 +0100

    navbar orizzontale con css

--- src/pagine/LayoutApp.tsx
+++ src/pagine/LayoutApp.tsx
@@ -4,14 +4,14 @@ const LayoutApp = () => {
   return (
     <>
       <nav>
-        <ul>
-          <li>
+        <ul className="ul-navbar">
+          <li className="li-navbar">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="li-navbar">
             <Link to="/gestione-lista">Gestione lista</Link>
           </li>
-          <li>
+          <li className="li-navbar">
             <Link to="/storico-carrelli">Storico carrelli</Link>
           </li>
         </ul>
