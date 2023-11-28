c6060525282bc049c5c6e01c311f3a8527ec34b7
Lorenzo Ferranti <lorenzoferranti04@outlook.it>
2023-11-28 10:33:39 +0100

    changed navbar css with flexbox

--- src/pagine/LayoutApp.tsx
+++ src/pagine/LayoutApp.tsx
@@ -5,15 +5,25 @@ const LayoutApp = () => {
     <>
       <nav>
         <ul className="ul-navbar">
+
           <li className="li-navbar">
-            <Link to="/">Home</Link>
+            <Link to="/" className="li-navbar-button" id="navbar-home-button">
+              Home
+            </Link>
           </li>
+
           <li className="li-navbar">
-            <Link to="/gestione-lista">Gestione lista</Link>
+            <Link to="/gestione-lista" className="li-navbar-button" id="navbar-gestione-lista-button">
+              Gestione lista
+            </Link>
           </li>
+
           <li className="li-navbar">
-            <Link to="/storico-carrelli">Storico carrelli</Link>
+            <Link to="/storico-carrelli" className="li-navbar-button" id="navbar-storico-carrelli-button">
+              Storico Carrelli
+            </Link>
           </li>
+
         </ul>
       </nav>
 
