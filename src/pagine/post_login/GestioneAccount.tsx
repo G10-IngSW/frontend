355a7dc86c7c41653eb55069a5574a9f671a1bf6
Andrea Denina <andredeninaf@gmail.com>
2023-12-07 02:40:58 +0100

    GestioneAccount (funzionamento da perfezionare)

--- src/pagine/post_login/GestioneAccount.tsx
+++ src/pagine/post_login/GestioneAccount.tsx
@@ -1,14 +1,247 @@
-import React, { useState } from 'react'
+import React, { FormEvent, useState } from 'react'
+import DatiApp from '../../classi/DatiApp'
+import { useAppContext } from '../../context';
 
 export const GestioneAccount = () => {
-  const [nomeAccount, setNomeAccount] = useState<string>("Mario");
+
+  const [modificaNome, setModificaNome] = useState(false);
+  const [modificaEmail, setModificaEmail] = useState(false);
+  const [modificaPassword, setModificaPassword] = useState(false);
+
+  const handleModificaNomeClick = () => {
+    setModificaNome(true);
+    setModificaEmail(false);
+    setModificaPassword(false);
+  };
+
+  const handleModificaEmailClick = () => {
+    setModificaNome(false);
+    setModificaEmail(true);
+    setModificaPassword(false);
+  };
+
+  const handleModificaPasswordClick = () => {
+    setModificaNome(false);
+    setModificaEmail(false);
+    setModificaPassword(true);
+  };
+
+  const handleCloseModificaNome = () => {
+    setModificaNome(false);
+  };
+
+  const handleCloseModificaEmail = () => {
+    setModificaEmail(false);
+  };
+
+  const handleCloseModificaPassword = () => {
+    setModificaPassword(false);
+  };
+
+  
 
   return (
     <div className="container-gestioneaccount">
-      <button className='gestioneaccount'>Cambia nome</button>
-      <button className='gestioneaccount'>Cambia email</button>
-      <button className='gestioneaccount'>Cambia password</button>
+      <button className='gestioneaccount' onClick={handleModificaNomeClick}>Cambia nome</button>
+      <button className='gestioneaccount' onClick={handleModificaEmailClick}>Cambia email</button>
+      <button className='gestioneaccount' onClick={handleModificaPasswordClick}>Cambia password</button>
       <button className='gestioneaccount'>Logout</button>
+
+      {modificaNome && <ModificaNomeForm onClose={handleCloseModificaNome} />}
+      {modificaEmail && <ModificaEmailForm onClose={handleCloseModificaEmail}/>}
+      {modificaPassword && <ModificaPasswordForm onClose={handleCloseModificaPassword}/>}
+
     </div>
+
   )
 }
+
+interface Props { 
+  onClose: () => void;
+}
+
+const ModificaNomeForm = ({onClose}: Props) => {
+  const [nuovoNome, setNuovoNome] = useState('');
+  const [password, setPassword] = useState('');
+  const datiApp = useAppContext();
+
+  const ModificaNome = async () => {
+    const idUtente = datiApp.account.id;
+    try {
+      const response = await fetch(`${datiApp.serverUrl}/utenti/modifica/${idUtente}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nome: nuovoNome,
+          vecchiaPassword: password,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Modifica effettuata con successo:', data);
+        datiApp.account.nome = nuovoNome;
+      } else {
+        const errorData = await response.json();
+        console.error('Errore durante la modifica:', errorData.error);
+      }
+    } catch (error) {
+      console.error('Errore durante la chiamata API:', error);
+    }
+  };
+
+  const handleSubmit = (e : FormEvent) => {
+    e.preventDefault();
+
+    ModificaNome();
+
+    setNuovoNome('');
+    setPassword('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nuovo Nome:
+        <input type="text" value={nuovoNome} onChange={(e) => setNuovoNome(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password di conferma:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Conferma Modifica</button>
+      <br />
+     <button type='button' onClick={onClose}>Chiudi</button>
+    </form>
+  );
+};
+
+
+
+const ModificaEmailForm = ({onClose}:Props) => {
+  const [nuovaEmail, setNuovaEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const datiApp = useAppContext();
+
+  const ModificaEmail = async () => {
+    const idUtente = datiApp.account.id;
+    try {
+      const response = await fetch(`${datiApp.serverUrl}/utenti/modifica/${idUtente}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: nuovaEmail,
+          vecchiaPassword: password,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Modifica effettuata con successo:', data);
+        datiApp.account.email = nuovaEmail;
+      } else {
+        const errorData = await response.json();
+        console.error('Errore durante la modifica:', errorData.error);
+      }
+    } catch (error) {
+      console.error('Errore durante la chiamata API:', error);
+    }
+  };
+
+  const handleSubmit = (e : FormEvent) => {
+    e.preventDefault();
+
+    ModificaEmail();
+
+    setNuovaEmail('');
+    setPassword('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nuova Email:
+        <input type="text" value={nuovaEmail} onChange={(e) => setNuovaEmail(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password di conferma:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Conferma Modifica</button>
+      <br />
+     <button type='button' onClick={onClose}>Chiudi</button>
+    </form>
+  );
+};
+
+const ModificaPasswordForm = ({onClose}: Props) => {
+  const [nuovaPassword, setNuovaPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const datiApp = useAppContext();
+
+  const ModificaPassword = async () => {
+    const idUtente = datiApp.account.id;
+    try {
+      const response = await fetch(`${datiApp.serverUrl}/utenti/modifica/${idUtente}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nuovaPassword: nuovaPassword,
+          vecchiaPassword: password,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Modifica effettuata con successo:', data);
+      } else {
+        const errorData = await response.json();
+        console.error('Errore durante la modifica:', errorData.error);
+      }
+    } catch (error) {
+      console.error('Errore durante la chiamata API:', error);
+    }
+  };
+
+
+
+  const handleSubmit = (e : FormEvent) => {
+    e.preventDefault();
+
+    ModificaPassword();
+
+    setNuovaPassword('');
+    setPassword('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Nuova Password:
+        <input type="password" value={nuovaPassword} onChange={(e) => setNuovaPassword(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password di conferma:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Conferma Modifica</button>
+      <br />
+     <button type='button' onClick={onClose}>Chiudi</button>
+    </form>
+  );
+};
+
+
+
