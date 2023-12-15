71a8d3d2bf641313a44b03e145beb6c87a68471a
Andrea Denina <andredeninaf@gmail.com>
2023-12-15 10:31:56 +0100

    switching to locale

--- src/pagine/post_login/LayoutApp.tsx
+++ src/pagine/post_login/LayoutApp.tsx
@@ -2,6 +2,7 @@ import { Outlet, Link, useNavigate } from "react-router-dom";
 import Header from "./componenti/Header";
 import DatiApp from "../../classi/DatiApp";
 import { useAppContext } from "../../context";
+import { useState } from "react";
 
 const LayoutApp = () => {
   return (
@@ -21,11 +22,11 @@ const NavBar = () => {
     <nav>
       <ul className="ul-navbar">
         <li className="li-navbar">
-        <NavbarButton route="/">Home</NavbarButton>
+        <NavbarButton route="/" idButton="0">Home</NavbarButton>
         </li>
 
         <li className="li-navbar">
-          <NavbarButton route="/liste">Liste</NavbarButton>
+          <NavbarButton route="/liste" idButton="1">Liste</NavbarButton>
         </li>
 
 
@@ -37,9 +38,10 @@ const NavBar = () => {
 interface Props {
   route: string;
   children: string;
+  idButton: string;
 }
 
-const NavbarButton = ({route, children}: Props) => {
+const NavbarButton = ({route, children,idButton}: Props) => {
 
   const datiApp = useAppContext();
   const navigate = useNavigate();
